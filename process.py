bb3aa1e9b1aa981626843f6b5b2549e7485d0688
Sarah Berg <sarahb0114@gmail.com>
2022-02-05 22:35:38 -0500

    Removed unnecessary methods

--- process.py
+++ process.py
@@ -1,7 +1,3 @@
-import csv
-
-
-# Should this actually be a class?
 class Process:
 
     def __init__(self, signer, company, image_path, vt_permalink, vt_detection=None):
@@ -10,21 +6,6 @@ class Process:
         self.image_path = image_path
         self.vt_detection = vt_detection
         self.vt_permalink = vt_permalink
-
-    def get_signer(self):
-        return self.signer
-
-    def get_company(self):
-        return self.company
-
-    def get_image_path(self):
-        return self.image_path
-
-    def get_vt_detection(self):
-        return self.vt_detection
-
-    def get_vt_permalink(self):
-        return self.vt_permalink
 
     # Display vt score and link
     def vt_str(self):
