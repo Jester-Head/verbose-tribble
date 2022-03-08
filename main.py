3670561f4c22ba6a1fff1f498d04012f20a693c8
Sarah Berg <sarahb0114@gmail.com>
2022-03-07 20:30:21 -0500

    main.py Fixed issue where on_modified ran too many times.

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 import csv
+import logging
 import time
+from datetime import datetime, timezone
+import fnmatch
 import watchdog
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from watchdog.events import FileSystemEventHandler, PatternMatchingEventHandler
 from prettytable import PrettyTable
 import process as p
 
 
-class Handler(watchdog.events.PatternMatchingEventHandler):
-    def __init__(self):
-        # Set the patterns for PatternMatchingEventHandler
-        watchdog.events.PatternMatchingEventHandler.__init__(self, patterns=['*.csv', '*.txt'],
-                                                             ignore_directories=True, case_sensitive=False)
+class Handler(FileSystemEventHandler):
 
     def on_created(self, event):
-        print("File created - % s." % event.src_path)
+        date = datetime.now()
+        fdate = date.strftime('%Y-%m-%d %H:%M:%S')
+        print(f'{fdate}: File created - {event.src_path}')
 
     def on_modified(self, event):
         """Creates new list of processes when file updates."""
-        print(f'File modified - {event.src_path}')
-        process_list = add_process(event)
-        display_processes(process_list)
+        time.sleep(1)
+        date = datetime.now()
+        fdate = date.strftime('%Y-%m-%d %H:%M:%S')
+        pattern = r'*done.txt'
+        if fnmatch.fnmatch(event.src_path, pattern):
+            print(f'{fdate} File modified - {event.src_path}')
+            process_list = add_process()
+            display_processes(process_list)
 
     def on_deleted(self, event):
-        print("File deleted - % s." % event.src_path)
+        date = datetime.now()
+        fdate = date.strftime('%Y-%m-%d %H:%M:%S')
+        print(f'{fdate}: File deleted - {event.src_path}')
 
 
-def add_process(event=None):
-    with open(rf'{event.src_path}', encoding='UTF-16') as csv_file:
+def add_process():
+    file = r'C:\logger\output.txt'
+    with open(rf'{file}', encoding='UTF-16') as csv_file:
         process_list = []
         process_list.clear()
         csv_reader = csv.DictReader(csv_file)
@@ -44,21 +53,14 @@ def add_process(event=None):
     return process_list
 
 
-def filter_score(process, threshold=1):
-    """Returns process with VT greater than or equal to threshold"""
-    if process.find_vt_score() >= threshold:
-        return process
-
-
-def display_processes(process_list, threshold=1):
+def display_processes(process_list, threshold=2):
     """Organizes processes into table"""
     my_table = PrettyTable(['Signer', 'Company', 'Image Path', 'VT detection', 'VT permalink'])
     for item in process_list:
-        filtered_item = filter_score(item, threshold)
-        if filtered_item is not None:
+        if item.find_vt_score() is not None and item.find_vt_score() >= threshold:
             my_table.add_row(
-                [filtered_item.signer, filtered_item.company, filtered_item.image_path, filtered_item.vt_detection,
-                 filtered_item.vt_permalink])
+                [item.signer, item.company, item.image_path, item.vt_detection,
+                 item.vt_permalink])
     print(my_table)
 
 
