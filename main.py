bcd4c517a490c26c15812a3b98846c15172941e8
Sarah Berg <sarahb0114@gmail.com>
2022-04-01 21:24:56 -0400

    Started basic web server

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import csv
-import logging
-import time
-from datetime import datetime, timezone
 import fnmatch
+import time
+from datetime import datetime
+
 import watchdog
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler, PatternMatchingEventHandler
 from prettytable import PrettyTable
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
 import process as p
+
+html_string1 = '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<title>Process ' \
+               'Display</title>\n</head>\n<body>\n '
+html_string2 = '\n</body>\n</html>'
 
 
 class Handler(FileSystemEventHandler):
@@ -26,7 +31,8 @@ class Handler(FileSystemEventHandler):
         if fnmatch.fnmatch(event.src_path, pattern):
             print(f'{fdate} File modified - {event.src_path}')
             process_list = add_process()
-            display_processes(process_list)
+            # display_processes(process_list)
+            write_processes_file(process_list)
 
     def on_deleted(self, event):
         date = datetime.now()
@@ -53,7 +59,7 @@ def add_process():
     return process_list
 
 
-def display_processes(process_list, threshold=2):
+def display_processes(process_list, threshold=1):
     """Organizes processes into table"""
     my_table = PrettyTable(['Signer', 'Company', 'Image Path', 'VT detection', 'VT permalink'])
     for item in process_list:
@@ -62,6 +68,20 @@ def display_processes(process_list, threshold=2):
                 [item.signer, item.company, item.image_path, item.vt_detection,
                  item.vt_permalink])
     print(my_table)
+
+
+def write_processes_file(process_list, threshold=1):
+    """Organizes processes into table"""
+    my_table = PrettyTable(['Signer', 'Company', 'Image Path', 'VT detection', 'VT permalink'])
+    for item in process_list:
+        if item.find_vt_score() is not None and item.find_vt_score() >= threshold:
+            my_table.add_row(
+                [item.signer, item.company, item.image_path, item.vt_detection,
+                 item.vt_permalink])
+    with open('index.html', 'w') as f:
+        f.write(html_string1 + my_table.get_html_string() + html_string2)
+        print(my_table.get_html_string())
+        print(my_table)
 
 
 if __name__ == "__main__":
